667112088fd8c0fcce4c4971d697e15caa4dfdb8
h80r <h80rgbc@gmail.com>
2022-06-12 20:49:45 -0300

    Finish password generation

--- bin/pass8.dart
+++ bin/pass8.dart
@@ -3,12 +3,22 @@ import 'package:pass8/pass8.dart';
 
 void main(List<String> arguments) {
   final wordbase = generateWordbase([
-    // 'UFT',
-    // 'Segurança e Auditoria de Sistemas',
-    // '2022',
+    'UFT',
+    'Segurança e Auditoria de Sistemas',
+    '2022',
     'Ciência da Computação'
-  ], 8, Modes.mixed);
+  ], passwordLength: 8, mode: Modes.mixed);
 
   print(wordbase);
   print(wordbase.length);
+
+  final passwords = generatePasswords(
+    wordbase,
+    passwordLength: 8,
+    passwordCount: 3000,
+    fillingMode: Modes.mixed,
+  );
+
+  print(passwords);
+  print(passwords.length);
 }
--- lib/pass8.dart
+++ lib/pass8.dart
@@ -4,6 +4,7 @@ import 'package:pass8/converters/leetify.dart';
 import 'package:pass8/generators/length_inator.dart';
 import 'package:pass8/generators/trim_inator.dart';
 import 'package:pass8/modes.dart';
+import 'package:trotter/trotter.dart';
 
 extension on List<String> {
   List<String> flatMap(List<String> Function(String) f) {
@@ -12,10 +13,10 @@ extension on List<String> {
 }
 
 List<String> generateWordbase(
-  List<String> input, [
-  int length = 8,
+  List<String> input, {
+  int? passwordLength = 8,
   Modes mode = Modes.shallow,
-]) {
+}) {
   print('Preparando lista de palavras...');
   final wordlist = input.map((e) => e.toLowerCase()).toSet().toList();
 
@@ -29,10 +30,36 @@ List<String> generateWordbase(
   final randomCase = leetSpeak.flatMap((w) => casefy(w, mode));
 
   print('Retirando espaços...');
-  final trimmed = trimInator(randomCase);
+  final trimmed = trimInator(randomCase)..shuffle();
 
-  print('Preenchendo senhas...');
-  final passwords = lengthInator(trimmed, length, mode);
+  if (passwordLength != null) {
+    print('Preenchendo senhas...');
+    final passwords = lengthInator(trimmed, passwordLength, mode)..shuffle();
+    return {...trimmed, ...passwords}.toList()..shuffle();
+  }
 
-  return passwords;
+  return trimmed;
+}
+
+List<String> generatePasswords(
+  List<String> wordbase, {
+  int? passwordLength = 8,
+  int passwordCount = 3000,
+  Modes fillingMode = Modes.shallow,
+}) {
+  final result = {...wordbase.where((w) => w.length >= (passwordLength ?? 0))};
+  final compounds = Compounds(wordbase);
+
+  var i = 0;
+  while (result.length < passwordCount) {
+    final word = compounds[i++].join();
+    if (word.length >= (passwordLength ?? 0)) {
+      result.add(word);
+      continue;
+    }
+
+    result.addAll(lengthInator([word], passwordLength!, fillingMode));
+  }
+
+  return result.toList();
 }
